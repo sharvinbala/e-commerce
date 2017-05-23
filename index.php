767e35a7ce88b8835a2d0307bfd224a09e3cff08
Your Name <you@example.com>
2017-05-23 18:05:29 +0800

    added the neccessary folders & bootstrap

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hitech Infinity</title>
+
+<link href="styles/boostrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="js/boostrap.min.js"></script>
+</body>
+</html>
