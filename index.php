9fb95207caeabaff6061c29ec73b0edc935fe130
Your Name <you@example.com>
2017-05-23 19:03:51 +0800

    Added navbar

--- index.php
+++ index.php
@@ -3,11 +3,60 @@
 <head>
 <title>Hitech Infinity</title>
 
-<link href="styles/boostrap.min.css" rel="stylesheet">
-
+<link href="http://fonts.googleapis.com/css?family=Roboto:500,500,700,300,100" rel="stylesheet">
+<link href="styles/bootstrap.min.css" rel="stylesheet">
+<link href="styles/style.css" rel="stylesheet">
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
 </head>
 
 <body>
+<div id="top"><!--top starts -->
+<div class="container"> <!--Container starts -->
+<div class ="col-md-6 offer"> <!-- col md 6 offer starts -->
+<a href="#" class="btn btn-success btn-sm">
+Welcome: Guest!
+</a>
+
+<a href="#">
+Shopping Cart Total Price: RM 100, Total Items: 2
+</a>
+
+</div><!--col md 6 offer ends -->
+
+<div class="col-md-6"> <!--col-md-6 starts -->
+<ul class="menu"><!--menu starts -->
+	<li>
+		<a href="customer_register.php">
+			Register
+		</a>
+	</li>
+
+	<li>
+		<a href="checkout.php">
+			My Account
+		</a>
+	</li>
+
+	<li>
+		<a href="cart.php">
+			Go To Cart
+		</a>
+	</li>
+
+	<li>
+		<a href="checkout.php">
+			Login
+		</a>
+	</li>
+
+</ul><!--menu ends -->
+
+</div><!--col-md-6 ends -->
+
+
+</div><!--Container ends -->
+
+</div><!--top ends -->
 
 
 
@@ -26,8 +75,7 @@
 
 
 
-
-
+<script type="js/jquery.min.js"></script>
 <script src="js/boostrap.min.js"></script>
 </body>
 </html>
