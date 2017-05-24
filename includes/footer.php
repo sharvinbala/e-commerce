ec61a1a0d610a816ac4972047240b60c5e8e10aa
Your Name <you@example.com>
2017-05-24 12:34:58 +0800

    created the rest of the footer but havent style

--- includes/footer.php
+++ includes/footer.php
@@ -18,6 +18,51 @@
 				<hr class="hidden-md hidden-lg hidden-sm">
 
 			</div><!-- col-md-3 col-sm-6 ends -->
+
+			<div class="col-md-3 col-sm-6"><!--col-md-3 col-sm-6 starts -->
+				<h4>Top Products Categories</h4>
+					<ul><!--ul starts -->
+						<li><a href="#">Basic Disposable Sets</a></li>
+						<li><a href="#">Surgery Sets</a></li>
+						<li><a href="#">Paedatric Sets</a></li>
+						<li><a href="#">Dental Sets</a></li>					
+					</ul><!--ul ends -->
+						<hr class="hidden-md hidden-lg">
+
+			</div><!--col-md-3 col-sm-6 ends -->
+
+			<div class="col-md-3 col-sm-6"><!--col-md-3 col-sm-6 starts -->
+				<h4>Where To Find Us</h4>
+				<p>
+					<strong>Hitech Infinity</strong>
+					<br>Subang Jaya
+					<br>Selangor.					
+				</p>
+
+				<a href="contact.php">Contact Us Page.</a>
+				<hr class="hidden-md hidden-lg">
+			</div><!--col-md-3 col-sm-6 ends -->
+
+			<div class="col-md-3 col-sm-6"><!--col-md-3 col-sm-6 starts -->
+
+				<h4>Get The News</h4>
+				<p class="text-muted">
+					We update new products every week!
+				</p>
+
+				<form action="" method="post"> <!--form starts -->
+					<div class="input-group"><!--input group starts -->
+						<input type="text" class="form-control" name="email">
+							<span class="input-group-btn">
+								<input type="submit" value="subscribe" class="btn btn-default">
+							</span>	
+
+					</div><!--input group ends -->
+
+				</form><!--form ends -->
+
+			</div><!--col-md-3 col-sm-6 ends -->
+
 		</div><!-- row ends -->
 	</div><!-- container starts -->
 </div><!-- Footer ends -->
