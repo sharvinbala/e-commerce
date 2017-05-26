c3e4c13aa994e58e93b5aa4bd3d53736012f70be
Your Name <you@example.com>
2017-05-27 00:26:47 +0800

    create insert_product.php continue inside form tag

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -8,8 +8,34 @@
 	</head>
 
 	<body>
+<div class="row"><!--row starts -->
+	<div class="col-lg-12"><!--col lg 12 starts -->
+		<ol class="breadcrumb"><!--breadcrumb starts -->
+			<li class="active">
+				<i class="fa fa-dashboard"></i> Dashboard / Insert Products
+			</li>
+		</ol><!--breadcrumb ends -->
+	</div><!--col lg 12 ends -->
+</div><!--row ends -->
 
+<div class="row"><!-- 2 row starts -->
+	<div class="col-lg-12"><!--col lg 12 starts -->
+		<div class="panel panel-default"><!--row starts --><!--panel default starts -->
+			<div class="panel-heading"><!--panel heading starts -->
+				<h3 class="panel-title">
+					<i class="fa fa-money fa-fw"></i> Insert Products
+				</h3>
+			</div><!--panel heading ends -->
 
+<div class="panel-body"><!--panel body starts -->
+	<form class="form-horizontal" method="post" enctype="multipart/form-data"><!--form horizontal starts -->
+		
+	</form><!--form horizontal ends -->
+</div><!--panel body ends -->			
+
+		</div><!--panel default ends -->
+	</div><!--col lg 12 ends -->
+</div><!-- 2 row ends -->
 
 
 
