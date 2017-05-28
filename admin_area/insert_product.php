f66cc2bf3858f7d5f0f02a4db303e08f29257485
Your Name <you@example.com>
2017-05-28 20:55:10 +0800

    created first echo product categories

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -29,7 +29,74 @@
 
 <div class="panel-body"><!--panel body starts -->
 	<form class="form-horizontal" method="post" enctype="multipart/form-data"><!--form horizontal starts -->
-		
+
+		<div class="form-group"><!--form-group starts -->
+			<label class="col-md-3 control-label">Products Title</label>
+				<div class="col-md-6"><!--col md 6 starts -->
+					<input type="text" name="product_title" class="form-control" required> 
+				</div><!--col md 6 ends -->
+		</div><!--form-group ends -->
+
+		<div class="form-group"><!--form-group starts -->
+			<label class="col-md-3 control-label">Products Category</label>
+				<div class="col-md-6"><!--col md 6 starts -->
+					<select name="product_cat" class="form-control">
+						<option>Select a Product Category</option>
+		<?php
+			$get_p_cats= "SELECT * FROM product_categories";
+			$run_p_cats = mysqli_query($con, $get_p_cats);
+
+			while($row_p_cats=mysqli_fetch_array($run_p_cats))
+			{
+				$p_cat_id = $row_p_cats['p_cat_id'];
+				$p_cat_title = $row_p_cats['p_cat_title'];
+
+				echo "
+					<option value='$p_cat_id'> $p_cat_title</option>
+				";
+
+			}
+
+		?>
+					</select>
+				</div><!--col md 6 ends -->
+		</div><!--form-group ends -->
+
+		<div class="form-group"><!--form-group starts -->
+			<label class="col-md-3 control-label">Products Title</label>
+				<div class="col-md-6"><!--col md 6 starts -->
+					<input type="text" name="product_title" class="form-control" required> 
+				</div><!--col md 6 ends -->
+		</div><!--form-group ends -->
+
+		<div class="form-group"><!--form-group starts -->
+			<label class="col-md-3 control-label">Products Title</label>
+				<div class="col-md-6"><!--col md 6 starts -->
+					<input type="text" name="product_title" class="form-control" required> 
+				</div><!--col md 6 ends -->
+		</div><!--form-group ends -->
+
+		<div class="form-group"><!--form-group starts -->
+			<label class="col-md-3 control-label">Products Title</label>
+				<div class="col-md-6"><!--col md 6 starts -->
+					<input type="text" name="product_title" class="form-control" required> 
+				</div><!--col md 6 ends -->
+		</div><!--form-group ends -->
+
+		<div class="form-group"><!--form-group starts -->
+			<label class="col-md-3 control-label">Products Title</label>
+				<div class="col-md-6"><!--col md 6 starts -->
+					<input type="text" name="product_title" class="form-control" required> 
+				</div><!--col md 6 ends -->
+		</div><!--form-group ends -->
+
+		<div class="form-group"><!--form-group starts -->
+			<label class="col-md-3 control-label">Products Title</label>
+				<div class="col-md-6"><!--col md 6 starts -->
+					<input type="text" name="product_title" class="form-control" required> 
+				</div><!--col md 6 ends -->
+		</div><!--form-group ends -->
+
 	</form><!--form horizontal ends -->
 </div><!--panel body ends -->			
 
