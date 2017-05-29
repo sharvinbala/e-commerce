493be0b6b849fbe1bd066f6cfacda3b619d6ecee
Your Name <you@example.com>
2017-05-29 14:21:50 +0800

    showing the products in each product category but when click its not showing the details of a particular product because its still static

--- functions/function.php
+++ functions/function.php
@@ -116,6 +116,33 @@ function getpcatpro()
 			";
 		}
 
+		while ($row_products = mysqli_fetch_array($run_products)) {
+			$pro_id = $row_products['product_id'];
+			$pro_title = $row_products['product_title'];
+			$pro_price = $row_products['product_price'];
+			$pro_img1 = $row_products['product_img1'];
+
+			echo "
+			<div class='col-md-4 col-sm-6 center-responsive'>
+				<div class='product'>
+					<a href='details.php?pro_id=$pro_id'>
+					<img src='admin_area/product_images/$pro_img1' class='img-responsive'>
+					</a>
+					<div class='text'>
+						<h3><a href='details.php?pro_id=$pro_id'> $pro_title</a></h3>
+						<p class='price'>RM $pro_price</p>
+						<p class='buttons'>
+							<a href='details.php?pro_id=$pro_id' class='btn btn-default'>View Details</a>
+							<a href='details.php?pro_id=$pro_id' class='btn btn-default'>
+							<i class='fa fa-shopping-cart'></i>Add to cart
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			";
+		}
+
 	}
 }
 
