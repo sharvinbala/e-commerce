f1a0cb9cce241951bb732993f37cf9b2809b5a1c
Your Name <you@example.com>
2017-05-30 22:29:22 +0800

    doing paying offline

--- payment_options.php
+++ payment_options.php
@@ -1,8 +1,21 @@
 <div class="box"><!-- box starts -->
+
+<?php
+	
+$session_email = $_SESSION['customer_email'];
+$select_customer = "select * from customers where customer_email='$session_email'";
+$run_customer = mysqli_query($con, $select_customer);
+$row_customer = mysqli_fetch_array($run_customer);
+$customer_id = $row_customer['customer_id']; 
+
+
+
+?>
+
 <h1 class="text-center">Payment Options</h1>
 <p class="lead text-center">
 
-<a href="#">Pay Offline</a>
+<a href="order.php?c_id=<?php echo $customer_id; ?>">Pay Offline</a>
 
 </p>
 
