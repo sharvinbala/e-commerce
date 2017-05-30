9c574bc0c556da4fd9c7d4375938527a0fecccb2
Your Name <you@example.com>
2017-05-30 18:19:30 +0800

    created customer_login.php and completed

--- customer/customer_login.php
+++ customer/customer_login.php
@@ -5,10 +5,33 @@
 <p class="lead">Already registered?</p>
 </center>
 <p class="text-muted">
-Click Here
+Login now to enjoy our special sales and discount offered during a limited time only!
 </p>
 </div><!--box-header ends -->
 
+<form action="checkout.php" method="post"><!--form starts -->
+<div class="form-group"><!--form-group starts -->
+<label>Email</label>
+<input type="text" class="form-control" name="c_email" required>
+</div><!--form-group ends -->
 
+<div class="form-group"><!--form-group starts -->
+<label>Password</label>
+<input type="password" class="form-control" name="c_pass" required>
+</div><!--form-group ends -->
+
+<div class="text-center"><!--text-center starts -->
+<button class="btn btn-primary" name="login" value="Login">
+<i class="fa fa-sign-in"></i> Log in
+</button>
+
+</div><!--text-center starts -->
+
+</form><!--form ends -->
+
+<center><!--center starts -->
+<a href="customer_register.php"></a>
+<h3>New? Register Here.</h3>
+</center><!--center ends -->
 
 </div><!--box ends-->
