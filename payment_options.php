2b2141d7492aca5d6e69397afec509ccb3af4cd2
Your Name <you@example.com>
2017-05-30 19:18:24 +0800

    created payment page with images

--- payment_options.php
+++ payment_options.php
@@ -1,1 +1,19 @@
-hhh
+<div class="box"><!-- box starts -->
+<h1 class="text-center">Payment Options</h1>
+<p class="lead text-center">
+
+<a href="#">Pay Offline</a>
+
+</p>
+
+<center><!-- center starts -->
+
+<p class="lead">
+<a href="#"> <!-- put paypal address here -->
+Pay Online With PayPal
+<img src="images/paypal.png" width="500" height="270" class="img-responsive">
+</a>
+</p>
+</center><!-- center ends -->
+
+</div><!-- box ends -->
