7181e9ffb8a9c018c09b34e5065605402e74004b
Your Name <you@example.com>
2017-05-30 18:55:32 +0800

    item in cart and then login will redirect to payment page

--- customer/customer_login.php
+++ customer/customer_login.php
@@ -35,3 +35,57 @@ Login now to enjoy our special sales and discount offered during a limited time 
 </center><!--center ends -->
 
 </div><!--box ends-->
+
+<?php
+
+if(isset($_POST['login'])){
+
+$customer_email = $_POST['c_email'];
+
+$customer_pass = $_POST['c_pass'];
+
+$select_customer = "select * from customers where customer_email='$customer_email' AND customer_pass='$customer_pass'";
+
+$run_customer = mysqli_query($con,$select_customer);
+
+$get_ip = getRealUserIp();
+
+$check_customer = mysqli_num_rows($run_customer);
+
+$select_cart = "select * from cart where ip_add='$get_ip'";
+
+$run_cart = mysqli_query($con,$select_cart);
+
+$check_cart = mysqli_num_rows($run_cart);
+
+if($check_customer==0){
+
+echo "<script>alert('password or email is wrong')</script>";
+
+exit();
+
+}
+
+if($check_customer==1 AND $check_cart==0){
+
+$_SESSION['customer_email']=$customer_email;
+
+echo "<script>alert('You are Logged In')</script>";
+
+echo "<script>window.open('customer/my_account.php?my_orders','_self')</script>";
+
+}
+else {
+
+$_SESSION['customer_email']=$customer_email;
+
+echo "<script>alert('You are Logged In')</script>";
+
+echo "<script>window.open('checkout.php','_self')</script>";
+
+} 
+
+
+}
+
+?>
