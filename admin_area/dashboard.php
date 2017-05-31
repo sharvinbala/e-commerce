5aa3918fab28d57f8df2c992f57a93886474447b
Your Name <you@example.com>
2017-05-31 21:44:31 +0800

    showing the blue panel

---
+++ admin_area/dashboard.php
@@ -0,0 +1,25 @@
+<div class="row"><!-- 1 row starts -->
+<div class="col-lg-12"><!-- col lg 12 starts -->
+<h1 class="page-header">Dashboard</h1>
+<ol class="breadcrumb"><!-- breadcrumb starts -->
+<li class="active">
+<i class="fa fa-dashboard"></i> Dashboard
+
+</li>
+</ol><!-- breadcrumb ends -->
+</div><!-- col lg 12 ends -->
+</div> <!-- 1 row ends -->
+
+<div class="row"><!-- 2 row starts -->
+<div class="col-lg-3 col-md-6"><!-- col md 6 starts -->
+<div class="panel panel-primary"><!-- panel primary starts -->
+<div class="panel-heading"><!-- panel heading starts -->
+<div class="row"><!--  panel heading row starts -->
+<div class="col-xs-3"><!-- col xs 3 starts -->
+<i class="fa fa-tasks fa-5x"></i>
+</div><!-- col xs 3 ends -->
+</div><!-- panel heading row ends -->
+</div><!-- panel heading ends -->
+</div><!-- panel primary ends -->
+</div><!-- col md 6 ends -->
+</div><!-- 2 row starts -->
--- admin_area/index.php
+++ admin_area/index.php
@@ -12,11 +12,19 @@
 <div id="wrapper"><!--wrapper starts -->
 <div id="page-wrapper"><!-- page wrapper starts -->
 <div class="container-fluid"><!--container fluid starts -->
+<?php
 
+if (isset($_GET['dashboard'])) {
+include("dashboard.php");
+	
+}
+
+
+?>
 <?php include ("includes/sidebar.php"); ?>
 
 </div><!--container fluid ends-->
-</div><!-- page wrapper starts -->
+</div><!-- page wrapper ends -->
 </div><!--wrapper ends -->
 
 <script src="js/jquery.min.js"></script>
