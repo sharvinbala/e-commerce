5ce2aafb081ee6d25c4f0d7b86dc48ba68b6ece1
Your Name <you@example.com>
2017-05-31 18:42:18 +0800

    doing admin panel first part nothing much

---
+++ admin_area/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Admin Panel</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<div id="wrapper"><!--wrapper starts -->
+<div id="page-wrapper"><!-- page wrapper starts -->
+<div class="container-fluid"><!--container fluid starts -->
+
+</div><!--container fluid ends-->
+</div><!-- page wrapper starts -->
+</div><!--wrapper ends -->
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
