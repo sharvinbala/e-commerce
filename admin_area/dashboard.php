706252bf952d31ce5e1a0784c684abd867adcfcc
Your Name <you@example.com>
2017-05-31 21:56:13 +0800

    all 4 panels in admin panel created

--- admin_area/dashboard.php
+++ admin_area/dashboard.php
@@ -18,8 +18,108 @@
 <div class="col-xs-3"><!-- col xs 3 starts -->
 <i class="fa fa-tasks fa-5x"></i>
 </div><!-- col xs 3 ends -->
+
+<div class="col-xs-9 text-right"><!-- col xs 9 text right starts -->
+<div class="huge">15</div>
+<div>Products</div>
+
+</div><!-- col xs 9 text right ends -->
+
 </div><!-- panel heading row ends -->
 </div><!-- panel heading ends -->
+
+<a href="index.php?view_products">
+<div class="panel-footer"><!-- panel footer starts -->
+<span class="pull-left"> View Details</span>
+<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+<div class="clearfix	"></div>
+</div><!-- panel footer ends -->
+</a>
 </div><!-- panel primary ends -->
 </div><!-- col md 6 ends -->
-</div><!-- 2 row starts -->
+
+
+<div class="col-lg-3 col-md-6"><!-- col md 6 starts -->
+<div class="panel panel-green"><!-- panel green starts -->
+<div class="panel-heading"><!-- panel heading starts -->
+<div class="row"><!--  panel heading row starts -->
+<div class="col-xs-3"><!-- col xs 3 starts -->
+<i class="fa fa-comments fa-5x"></i>
+</div><!-- col xs 3 ends -->
+
+<div class="col-xs-9 text-right"><!-- col xs 9 text right starts -->
+<div class="huge">9</div>
+<div>Customers</div>
+
+</div><!-- col xs 9 text right ends -->
+
+</div><!-- panel heading row ends -->
+</div><!-- panel heading ends -->
+
+<a href="index.php?view_customers">
+<div class="panel-footer"><!-- panel footer starts -->
+<span class="pull-left"> View Details</span>
+<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+<div class="clearfix	"></div>
+</div><!-- panel footer ends -->
+</a>
+</div><!-- panel green ends -->
+</div><!-- col md 6 ends -->
+
+<div class="col-lg-3 col-md-6"><!-- col md 6 starts -->
+<div class="panel panel-yellow"><!-- panel yellow starts -->
+<div class="panel-heading"><!-- panel heading starts -->
+<div class="row"><!--  panel heading row starts -->
+<div class="col-xs-3"><!-- col xs 3 starts -->
+<i class="fa fa-shopping-cart fa-5x"></i>
+</div><!-- col xs 3 ends -->
+
+<div class="col-xs-9 text-right"><!-- col xs 9 text right starts -->
+<div class="huge">6</div>
+<div>Product Categories</div>
+
+</div><!-- col xs 9 text right ends -->
+
+</div><!-- panel heading row ends -->
+</div><!-- panel heading ends -->
+
+<a href="index.php?view_cats">
+<div class="panel-footer"><!-- panel footer starts -->
+<span class="pull-left"> View Details</span>
+<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+<div class="clearfix	"></div>
+</div><!-- panel footer ends -->
+</a>
+</div><!-- panel yellow ends -->
+</div><!-- col md 6 ends -->
+
+<div class="col-lg-3 col-md-6"><!-- col md 6 starts -->
+<div class="panel panel-red"><!-- panel red starts -->
+<div class="panel-heading"><!-- panel heading starts -->
+<div class="row"><!--  panel heading row starts -->
+<div class="col-xs-3"><!-- col xs 3 starts -->
+<i class="fa fa-support fa-5x"></i>
+</div><!-- col xs 3 ends -->
+
+<div class="col-xs-9 text-right"><!-- col xs 9 text right starts -->
+<div class="huge">4</div>
+<div>Orders</div>
+
+</div><!-- col xs 9 text right ends -->
+
+</div><!-- panel heading row ends -->
+</div><!-- panel heading ends -->
+
+<a href="index.php?view_orders">
+<div class="panel-footer"><!-- panel footer starts -->
+<span class="pull-left"> View Details</span>
+<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+<div class="clearfix	"></div>
+</div><!-- panel footer ends -->
+</a>
+</div><!-- panel red ends -->
+</div><!-- col md 6 ends -->
+
+
+
+</div><!-- 2 row ends -->
