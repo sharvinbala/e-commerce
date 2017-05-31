fbf4a406b990d48dae119431a69d1a16e836f54e
Your Name <you@example.com>
2017-05-31 19:07:58 +0800

    top right dropdown bar basic

--- admin_area/includes/sidebar.php
+++ admin_area/includes/sidebar.php
@@ -12,4 +12,53 @@
 <a class="navbar-brand" href="index.php?dashboard">Admin Panel</a>
 
 </div><!--navbar-header ends -->
+
+<ul class="nav navbar-right top-nav"><!-- navbar right starts -->
+<li class="dropdown"><!-- dropdown starts -->
+<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+<i class="fa fa-user"></i>
+Sharvin <b class="caret"></b>
+</a>
+
+<ul class="dropdown-menu"><!-- dropdown-menu starts -->
+
+<li><!-- li starts -->
+<a href="index.php?user_profile">
+<i class="fa fa-fw fa-user"></i>Profile
+
+</a>
+</li><!-- li ends -->
+
+<li><!-- li starts -->
+<a href="index.php?view_products">
+<i class="fa fa-fw fa-envelope"></i>Products
+
+</a>
+</li><!-- li ends -->
+
+<li><!-- li starts -->
+<a href="index.php?user_profile">
+<i class="fa fa-fw fa-user"></i>Products
+<span class="badge">7</span>
+</a>
+</li><!-- li ends -->
+
+<li><!-- li starts -->
+<a href="index.php?view_customers">
+<i class="fa fa-fw fa-gear"></i>Customers
+<span class="badge">12</span>
+
+<li><!-- li starts -->
+<a href="index.php?view_cats">
+<i class="fa fa-fw fa-gear"></i>Product Categories
+<span class="badge">5</span>
+
+</a>
+</li><!-- li ends -->
+
+</ul><!-- dropdown menu ends -->
+
+</li><!-- dropdown ends -->
+</ul><!-- navbar right ends -->
+
 </nav> <!--navbar ends -->
