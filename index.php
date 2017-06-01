0162fdb3874e8fe36337a6129b3ab0d48a3d07ec
Your Name <you@example.com>
2017-06-01 22:08:43 +0800

    slider, view categories, view products done..inside slider tabe in database changed slide_int to slide_id (could be a reason why its not showing correctly) and also edited some code in index.php in carousel part

--- index.php
+++ index.php
@@ -193,9 +193,9 @@
 
 			<ol class="carousel-indicators"><!-- carousel indicators starts -->
 				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-				<li data-target="#myCarousel" data-slide-to="1"</li>
-				<li data-target="#myCarousel" data-slide-to="2"</li>
-				<li data-target="#myCarousel" data-slide-to="3"</li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+				<li data-target="#myCarousel" data-slide-to="3"></li>		
 			</ol> <!-- carousel indicators ends -->
 
 <div class="carousel-inner"><!-- carousel inner starts -->
