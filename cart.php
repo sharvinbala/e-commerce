91e15f2ba2f70930f84895f98e9d6f2dba584a24
Your Name <you@example.com>
2017-06-05 17:28:41 +0800

    coupon code is working

--- cart.php
+++ cart.php
@@ -274,6 +274,16 @@
 										</tr>
 									</tfoot><!--tfoot ends -->
 							</table><!--table ends -->
+<div class="form-inline pull-right"><!-- form inline pull right starts -->
+<div class="form-group"><!-- form group starts -->
+<label>Coupon Code: </label>
+<input type="text" name="code" class="form-control">
+</div><!-- form group ends -->
+<input class="btn btn-primary" type="submit" name="apply_coupon"
+value="Apply Coupon">
+</div><!-- form inline pull right ends -->
+
+
 						</div><!--table responsive ends -->
 
 						<div class="box-footer"><!--box-footer starts -->
@@ -295,6 +305,89 @@ Proceed To Checkout <i class="fa fa-chevron-right"></i>
 						</div><!--box-footer ends -->
 				</form><!--form ends -->
 			</div><!-- box ends -->
+
+<?php
+
+if(isset($_POST['apply_coupon'])){
+
+$code = $_POST['code'];
+
+if($code == ""){
+
+
+}
+else{
+
+$get_coupons = "select * from coupons where coupon_code='$code'";
+
+$run_coupons = mysqli_query($con,$get_coupons);
+
+$check_coupons = mysqli_num_rows($run_coupons);
+
+if($check_coupons == 1){
+
+$row_coupons = mysqli_fetch_array($run_coupons);
+
+$coupon_pro = $row_coupons['product_id'];
+
+$coupon_price = $row_coupons['coupon_price'];
+
+$coupon_limit = $row_coupons['coupon_limit'];
+
+$coupon_used = $row_coupons['coupon_used'];
+
+
+if($coupon_limit == $coupon_used){
+
+echo "<script>alert('Your Coupon Code Has Been Expired')</script>";
+
+}
+else{
+
+$get_cart = "select * from cart where p_id='$coupon_pro' AND ip_add='$ip_add'";
+
+$run_cart = mysqli_query($con,$get_cart);
+
+$check_cart = mysqli_num_rows($run_cart);
+
+
+if($check_cart == 1){
+
+$add_used = "update coupons set coupon_used=coupon_used+1 where coupon_code='$code'";
+
+$run_used = mysqli_query($con,$add_used);
+
+$update_cart = "update cart set p_price='$coupon_price' where p_id='$coupon_pro' AND ip_add='$ip_add'";
+
+$run_update = mysqli_query($con,$update_cart);
+
+echo "<script>alert('Your Coupon Code Has Been Applied')</script>";
+
+echo "<script>window.open('cart.php','_self')</script>";
+
+}
+else{
+
+echo "<script>alert('Product Does Not Exist In Cart')</script>";
+
+}
+
+}
+
+}
+else{
+
+echo "<script> alert('Your Coupon Code Is Not Valid') </script>";
+
+}
+
+}
+
+
+}
+
+
+?>
 
 			<?php 
 				function update_cart()
